5723cc0f6f7bba882763f903049001ffb815b78a
Luís Cury <luis.cury@outlook.com>
2021-03-01 20:13:11 -0300

    Change updatePressure.

--- structures/vascularElements/SingleVessel.cpp
+++ structures/vascularElements/SingleVessel.cpp
@@ -74,9 +74,12 @@ void SingleVessel::updatePressure() {
 	double radiusSqr = this->radius * this->radius;
 	this->localResistance = (8 * this->viscosity * this->length) / (M_PI * radiusSqr *  radiusSqr);
 	double distalPressure = 0;
-	for (std::vector<AbstractVascularElement *>::iterator it = children.begin(); it != children.end(); ++it) {
-		distalPressure += (*it)->getProximalPressure();
+	if (!this->children.empty()) {
+		distalPressure =  this->children.at(0)->getProximalPressure();
 	}
+	/* for (std::vector<AbstractVascularElement *>::iterator it = children.begin(); it != children.end(); ++it) {
+		distalPressure += (*it)->getProximalPressure();
+	} */
 	this->pressure = this->flow * this->localResistance + distalPressure;
 }
 
